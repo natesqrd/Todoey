f42f6e237b8a253e4c3d8013fec7120ba316f062
Nathan <diicky@gmail.com>
2019-09-29 15:49:32 -0700

    Added Viewtable, addItems, Checkmarking

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Nathaniel Tucker on 9/28/19.
+//  Copyright © 2019 Nathaniel Tucker. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController
+{
+    //MARK: - Global Variables
+    var itemArray = [String]()
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    //MARK: - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    //MARK: - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    //MARK: - Add New Item
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem)
+    {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default)
+        {
+            (action) in
+            if textField.text != ""
+            {
+                self.itemArray.append(textField.text!)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
